1722717399b43fa5788d6c9edf758ce3b65b5c94
Aniket Shahi <109075043+aniket0608@users.noreply.github.com>
2023-11-13 20:29:29 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/code.jpg";
+import image from "../images/Code.jpg";
 
 const imageAltText = "Background image";
 
